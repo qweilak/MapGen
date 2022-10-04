155bee41df6ddfb03bc46233408197a335ae35bd
Jesús Mosquera Mayo <qweilak@gmail.com>
2022-10-04 15:55:40 +0200

    comentarios
    comentarios en script

--- Assets/Scripts/MyRotation.cs
+++ Assets/Scripts/MyRotation.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class MyRotation : MonoBehaviour
 {
+    //Variable de la velocidad de giro
     [SerializeField] [Range(0.1f, 0.5f)] float _angularSpeed;
 
     // Update is called once per frame
     void Update()
     {
+        //Calculo rotacion
         Vector3 rotation = Vector3.zero;
         rotation.y= _angularSpeed;
-
+        //Aplicar rotacion
         transform.Rotate(rotation);
     }
 }
