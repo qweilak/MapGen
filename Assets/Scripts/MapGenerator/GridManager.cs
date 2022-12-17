b3d24028d2425226dfb5ffc9022f57beba4f4a22
Jesús Mosquera Mayo <qweilak@gmail.com>
2022-12-17 17:30:54 +0100

    Coments added
    Quads with names and code coments added.

--- Assets/Scripts/MapGenerator/GridManager.cs
+++ Assets/Scripts/MapGenerator/GridManager.cs
@@ -8,28 +8,35 @@ namespace MapGenerator
     {
         [SerializeField]
         private int _colums, _rows;
-
         // Start is called before the first frame update
         void Start()
         {
-            _rows = (int) Camera.main.orthographicSize * 2 + 1;
+            //grid size picked from camera size and aspect.
+            _rows   = (int) Camera.main.orthographicSize * 2 + 1;
             _colums = (int) (_rows * Camera.main.aspect);
 
+            //new grid 
             for (int x = 0; x < _colums; x++)
-            {
                 for (int y = 0; y < _rows; y++)
+                { 
                     SpawnTile(new Vector2(x, y));
-            }
+                    
+                }
+
+            //We need to move the camera to the center of the grid. z set as default
+            Camera.main.transform.position = new Vector3(_colums /2 , _rows / 2, -10);
         }
 
+        /// <summary>
+        /// SpawnTile instantiates a new Quad at parameter's position.
+        /// </summary>
+        /// <param name="position"></param>
         private void SpawnTile(Vector2 position) 
         {
-            GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            
-            position.x = position.x - _colums / 2;
-            position.y = position.y - _rows / 2;
-
+            //Based on Quads.
+            GameObject quad         = GameObject.CreatePrimitive(PrimitiveType.Quad);
             quad.transform.position = position;
+            quad.name = "x: " + position.x + ", y: " + position.y;
         }
     }
 }
