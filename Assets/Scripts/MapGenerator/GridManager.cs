4db8c1da153ff613889fd03812db59067041029a
Jesús Mosquera Mayo <qweilak@gmail.com>
2022-12-17 18:28:07 +0100

    From quads to sprites.
    SpawnTile had changed to instantiate sprites instead of Quads. And Adds a gray scale value to each sprite.

--- Assets/Scripts/MapGenerator/GridManager.cs
+++ Assets/Scripts/MapGenerator/GridManager.cs
@@ -1,42 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MapGenerator
 {
     public class GridManager : MonoBehaviour
     {
+        //tile prefab to render
         [SerializeField]
+        [Tooltip("This white sprite must be refenced and set to 32 pixels per unit")]
+        private Sprite _sprite;
+        // Rows and colums of the grid
         private int _colums, _rows;
+
         // Start is called before the first frame update
         void Start()
         {
-            //grid size picked from camera size and aspect.
+            // Grid size picked from camera size and aspect.
             _rows   = (int) Camera.main.orthographicSize * 2 + 1;
             _colums = (int) (_rows * Camera.main.aspect);
 
-            //new grid 
+            // New grid 
             for (int x = 0; x < _colums; x++)
                 for (int y = 0; y < _rows; y++)
                 { 
-                    SpawnTile(new Vector2(x, y));
-                    
+                    float greyRandomValue = Random.Range(0.0f, 1.0f);
+                    SpawnTile(new Vector2(x, y), greyRandomValue);
                 }
-
-            //We need to move the camera to the center of the grid. z set as default
-            Camera.main.transform.position = new Vector3(_colums /2 , _rows / 2, -10);
         }
 
         /// <summary>
-        /// SpawnTile instantiates a new Quad at parameter's position.
+        /// SpawnTile instantiates a new Tile from parameter's position with offset.
+        /// The 0 position of the grid will be set at the bottom left screen's corner.
         /// </summary>
-        /// <param name="position"></param>
-        private void SpawnTile(Vector2 position) 
+        /// <param name="position"> Vector3 position of the tile </param>
+        /// <param name="value"> float from 0 to 1 </param>
+        private void SpawnTile(Vector2 position, float value) 
         {
-            //Based on Quads.
-            GameObject quad         = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            quad.transform.position = position;
-            quad.name = "x: " + position.x + ", y: " + position.y;
+            //Move each tile from screen's center to bottom left.
+            position.x -= _colums/2;
+            position.y -= _rows  / 2;
+            
+            // Instantiate tile with name.
+            GameObject tile = new GameObject("x: " + position.x + ", y: " + position.y);
+            //and set position 
+            tile.transform.position = position;
+
+            // Adding render and color
+            SpriteRenderer sRender = tile.AddComponent<SpriteRenderer>();
+            sRender.sprite = _sprite;
+            sRender.color = new Color(value,value,value, 1);
         }
     }
 }
