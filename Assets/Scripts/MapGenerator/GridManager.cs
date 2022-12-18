9588f1009bfc285994a22eb6c6a483b21ecbc5ba
Jesús Mosquera Mayo <qweilak@gmail.com>
2022-12-18 13:52:52 +0100

    Refactoring code
    Refactoring code with a new method to get the wolrd position's tile.

--- Assets/Scripts/MapGenerator/GridManager.cs
+++ Assets/Scripts/MapGenerator/GridManager.cs
@@ -1,12 +1,11 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 namespace MapGenerator
 {
     public class GridManager : MonoBehaviour
     {
         //tile prefab to render
-        [SerializeField]
-        [Tooltip("This white sprite must be refenced and set to 32 pixels per unit")]
+        [SerializeField][Tooltip("This white sprite must be refenced and set to 32 pixels per unit")]
         private Sprite _sprite;
         // Rows and colums of the grid
         private int _colums, _rows;
@@ -16,7 +15,7 @@ namespace MapGenerator
         {
             // Grid size picked from camera size and aspect.
             _rows   = (int) Camera.main.orthographicSize * 2 + 1;
-            _colums = (int) (_rows * Camera.main.aspect);
+            _colums = (int) (_rows * Camera.main.aspect) + 1;
 
             // New grid 
             for (int x = 0; x < _colums; x++)
@@ -28,17 +27,31 @@ namespace MapGenerator
         }
 
         /// <summary>
-        /// SpawnTile instantiates a new Tile related to the bottom left screen's corner.
-        /// <para name="position">• vector3 position: sets position of the tile.</para>
-        /// <para name="value">• float value: set the grey scale from 0 to 1.</para>
+        /// GridWorldPosition returns a vector3 with the realworld position of a tile's grid.
+        /// <list><em>
+        /// <param name="position">position is the grid position given by a vector2.</param>
+        /// </em></list>
+        /// <returns name="position"><strong>Returns tile's world position with position 0 set 
+        /// at botton left corner of the screen.</strong></returns>
         /// </summary>
+        private Vector3 GridWorldPosition(Vector2 position) 
+        {
+            //each tile will have a relative position to the screen's left bottom corner.
+            position.x -= _colums / 2;
+            position.y -= _rows / 2;
+            return position;
+        }
 
+        /// <summary>
+        /// SpawnTile instantiates a new Tile.
+        /// <list><em>
+        /// <para name="position">position: sets position of the tile.</para>
+        /// <para name="value">value: set the grey scale from 0 to 1.</para>
+        /// </em></list>
+        /// </summary>
         private void SpawnTile(Vector2 position, float value) 
         {
-            //Move each tile from screen's center to bottom left.
-            position.x -= _colums/2;
-            position.y -= _rows  / 2;
-            
+            position = GridWorldPosition(position);
             // Instantiate tile with name.
             GameObject tile = new GameObject("x: " + position.x + ", y: " + position.y);
             //and set position 
@@ -49,5 +62,6 @@ namespace MapGenerator
             sRender.sprite = _sprite;
             sRender.color = new Color(value,value,value, 1);
         }
+
     }
 }
