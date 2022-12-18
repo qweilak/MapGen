35a0fab8bdea738634334b17e20830c0bf99f09f
Jesús Mosquera Mayo <qweilak@gmail.com>
2022-12-18 16:55:59 +0100

    instatiate tiles from spritesheet
    Now the code instatiates a sprite relative to the position of the tile.

--- Assets/Scripts/MapGenerator/GridManager.cs
+++ Assets/Scripts/MapGenerator/GridManager.cs
@@ -5,8 +5,8 @@ namespace MapGenerator
     public class GridManager : MonoBehaviour
     {
         //tile prefab to render
-        [SerializeField][Tooltip("This white sprite must be refenced and set to 32 pixels per unit")]
-        private Sprite _sprite;
+        [SerializeField][Tooltip("Sprites must be refenced and the spritesheet must be set to 16 pixels per unit")]
+        private Sprite[] _sprites;
         // Rows and colums of the grid
         private int _colums, _rows;
 
@@ -14,16 +14,13 @@ namespace MapGenerator
         void Start()
         {
             // Grid size picked from camera size and aspect.
-            _rows   = (int) Camera.main.orthographicSize * 2 + 1;
+            _rows   = (int) Camera.main.orthographicSize * 2 - 1;
             _colums = (int) (_rows * Camera.main.aspect) + 1;
 
             // New grid 
             for (int x = 0; x < _colums; x++)
                 for (int y = 0; y < _rows; y++)
-                { 
-                    float greyRandomValue = Random.Range(0.0f, 1.0f);
-                    SpawnTile(new Vector2(x, y), greyRandomValue);
-                }
+                    SpawnTile(new Vector2(x, y));
         }
 
         /// <summary>
@@ -46,21 +43,41 @@ namespace MapGenerator
         /// SpawnTile instantiates a new Tile.
         /// <list><em>
         /// <para name="position">position: sets position of the tile.</para>
-        /// <para name="value">value: set the grey scale from 0 to 1.</para>
         /// </em></list>
         /// </summary>
-        private void SpawnTile(Vector2 position, float value) 
+        private void SpawnTile(Vector2 position) 
         {
+            int index = GetTyleType(position);
+
             position = GridWorldPosition(position);
             // Instantiate tile with name.
             GameObject tile = new GameObject("x: " + position.x + ", y: " + position.y);
             //and set position 
             tile.transform.position = position;
 
-            // Adding render and color
+            // Adding render
             SpriteRenderer sRender = tile.AddComponent<SpriteRenderer>();
-            sRender.sprite = _sprite;
-            sRender.color = new Color(value,value,value, 1);
+            sRender.sprite = _sprites[index];
+        }
+
+        /// <summary>
+        /// GetTyleType returns int as index of _sprites Array. Coded by the sides of the ends.
+        /// <list><em>
+        /// <param name="pos">vector2 position: pass the tile's position in world map.</param>
+        /// </em></list>
+        /// <returns><strong>returns int between 0 and _sprites.Lenght.</strong></returns>
+        /// </summary>
+        private int GetTyleType(Vector2 pos) 
+        {
+            if (pos.x == 0 && pos.y == 0) return 6;                        // bottom left
+            else if (pos.x == _colums - 1 && pos.y == _rows - 1) return 2; // top right
+            else if (pos.x == _colums - 1 && pos.y == 0) return 8;         // top left
+            else if (pos.x == 0 && pos.y == _rows - 1) return 0;           // bottom right
+            else if (pos.x == 0) return 3;                                 // left line
+            else if (pos.x == _colums - 1) return 5;                       // right line
+            else if (pos.y == 0) return 7;                                 // bottom line
+            else if (pos.y == _rows - 1) return 1;                         // top line
+            return 4;                                                      // center
         }
 
     }
